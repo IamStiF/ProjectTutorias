f656f63ecaf21646b949b73a0cfe2d91e9bb65ab
IamStiF <90409830+IamStiF@users.noreply.github.com>
2023-07-23 11:17:38 -0600

    Descripción de los cambios

--- src/components/Topics.jsx
+++ src/components/Topics.jsx
@@ -1,93 +1,107 @@
-import '../Topics.css'
+import React, { useState } from 'react';
+import '../Topics.css';
 
 export function Topics() {
+  const [showContent, setShowContent] = useState(false);
 
-    return (
-      <>
-        <div className='my-3'>
-           <h2 className="h1">Topics</h2> 
+  const handleShowContent = () => {
+    setShowContent(true);
+  };
+
+  const handleHideContent = () => {
+    setShowContent(false);
+  };
+
+  return (
+    <>
+      <div className='my-3'>
+        <h2 className="h1">Topics</h2>
+      </div>
+
+      {showContent ? (
+        <div>
+          <h1>Contenido adicional mostrado</h1>
+          <button onClick={handleHideContent}>Ocultar Contenido</button>
         </div>
-
+      ) : (
         <div id="home_quicklinks">
-  
-  <div>
-  <a class="quicklink link3" href="#">
-    <span class="ql_caption">
-      <span class="outer">
-        <span class="inner">
-          <h2>Relations</h2>
-        </span>
-      </span>
-    </span>
-    <span class="ql_top"></span>
-    <span class="ql_bottom"></span>
-  </a>
+          <div>
+            <button className="quicklink link3" onClick={handleShowContent}>
+              <span className="ql_caption">
+                <span className="outer">
+                  <span className="inner">
+                    <h2>Relations</h2>
+                  </span>
+                </span>
+              </span>
+              <span className="ql_top"></span>
+              <span className="ql_bottom"></span>
+            </button>
 
-  <a class="quicklink link3" href="#">
-    <span class="ql_caption">
-      <span class="outer">
-        <span class="inner">
-          <h2>Pointers</h2>
-        </span>
-      </span>
-    </span>
-    <span class="ql_top"></span>
-    <span class="ql_bottom"></span>
-  </a>
+            <button className="quicklink link3" onClick={handleShowContent}>
+              <span className="ql_caption">
+                <span className="outer">
+                  <span className="inner">
+                    <h2>Pointers</h2>
+                  </span>
+                </span>
+              </span>
+              <span className="ql_top"></span>
+              <span className="ql_bottom"></span>
+            </button>
 
-  <a class="quicklink link3" href="#">
-    <span class="ql_caption">
-      <span class="outer">
-        <span class="inner">
-          <h2>Arrays</h2>
-        </span>
-      </span>
-    </span>
-    <span class="ql_top"></span>
-    <span class="ql_bottom"></span>
-  </a>
-  </div>
+            <button className="quicklink link3" onClick={handleShowContent}>
+              <span className="ql_caption">
+                <span className="outer">
+                  <span className="inner">
+                    <h2>Arrays</h2>
+                  </span>
+                </span>
+              </span>
+              <span className="ql_top"></span>
+              <span className="ql_bottom"></span>
+            </button>
+          </div>
 
-<div className='my-5'>
-  <a class="quicklink link3" href="#">
-    <span class="ql_caption">
-      <span class="outer">
-        <span class="inner">
-          <h2>Lists</h2>
-        </span>
-      </span>
-    </span>
-    <span class="ql_top"></span>
-    <span class="ql_bottom"></span>
-  </a>
+          <div className='my-5'>
+            <button className="quicklink link3" onClick={handleShowContent}>
+              <span className="ql_caption">
+                <span className="outer">
+                  <span className="inner">
+                    <h2>Lists</h2>
+                  </span>
+                </span>
+              </span>
+              <span className="ql_top"></span>
+              <span className="ql_bottom"></span>
+            </button>
 
-  <a class="quicklink link3 " href="#">
-    <span class="ql_caption">
-      <span class="outer">
-        <span class="inner">
-          <h2>inheritance </h2>
-        </span>
-      </span>
-    </span>
-    <span class="ql_top"></span>
-    <span class="ql_bottom"></span>
-  </a>
+            <button className="quicklink link3" onClick={handleShowContent}>
+              <span className="ql_caption">
+                <span className="outer">
+                  <span className="inner">
+                    <h2>Inheritance</h2>
+                  </span>
+                </span>
+              </span>
+              <span className="ql_top"></span>
+              <span className="ql_bottom"></span>
+            </button>
 
-  <a class="quicklink link3" href="#">
-    <span class="ql_caption">
-      <span class="outer">
-        <span class="inner">
-          <h2>Diagrams</h2>
-        </span>
-      </span>
-    </span>
-    <span class="ql_top"></span>
-    <span class="ql_bottom"></span>
-  </a>
-  </div>
-</div>
-
-
-      </>
-    )
-  }
+            <button className="quicklink link3" onClick={handleShowContent}>
+              <span className="ql_caption">
+                <span className="outer">
+                  <span className="inner">
+                    <h2>Diagrams</h2>
+                  </span>
+                </span>
+              </span>
+              <span className="ql_top"></span>
+              <span className="ql_bottom"></span>
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
